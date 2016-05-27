ccb05ebfa6a05463b6b66e9411d6fa036d8a0de7
YangJian <229201093@qq.com>
2016-05-27 15:02:19 +0800

    Update

--- YJNumberFromat/ViewController.m
+++ YJNumberFromat/ViewController.m
@@ -7,8 +7,11 @@
 //
 
 #import "ViewController.h"
+#import "UITextField+Helper.h"
 
-@interface ViewController ()
+@interface ViewController ()<UITextFieldDelegate>
+@property (weak, nonatomic) IBOutlet UITextField *phoneTextField;
+@property (weak, nonatomic) IBOutlet UITextField *bankCardTextField;
 
 @end
 
@@ -16,12 +19,27 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    self.phoneTextField.delegate = self;
+    self.bankCardTextField.delegate = self;
+}
+
+
+#pragma mark - UITextFieldDelegate
+-(BOOL)textField:(UITextField *)textField shouldChangeCharactersInRange:(NSRange)range replacementString:(NSString *)string
+{
+    if (self.phoneTextField == textField)
+    {
+        return [UITextField phoneNumberFormatTextField:self.phoneTextField shouldChangeCharactersInRange:range replacementString:string];
+    }
+    else
+    {
+        return [UITextField bankNumberFormatTextField:self.bankCardTextField shouldChangeCharactersInRange:range replacementString:string];
+    }
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    
 }
 
 @end
